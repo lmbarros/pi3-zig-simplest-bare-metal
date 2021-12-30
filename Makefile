515a1907c3d717c56d3042835bc03932780b89f4
Leandro Motta Barros <lmb@stackedboxes.org>
2021-12-29 23:09:57 -0300

    Blink the LED
    Ugly, ugly way to delay execution for a while, but oh yeah, it blinks!

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@ all:
 	arm-none-eabi-objcopy -j .text.boot -j .text zig-out/bin/simplest -O binary kernel7.img
 
 
-dump:
+dump: all
 	arm-none-eabi-objdump -b binary -D kernel7.img -m arm
--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,9 @@
-const GPFSEL1: *u32 = @intToPtr(*u32, 0x3F20_0004);
-const GPFSET0: *u32 = @intToPtr(*u32, 0x3F20_001C);
-
-// comptime {
-//     asm (
-//         \\.global my_func;
-//         \\.type my_func, @function;
-//         \\my_func:
-//         \\  lea (%rdi,%rsi,1),%eax
-//         \\  retq
-//     );
-// }
+const GPFSEL1 = @intToPtr(*volatile u32, 0x3F20_0004);
+const GPSET0 = @intToPtr(*volatile u32, 0x3F20_001C);
+const GPCLR0 = @intToPtr(*volatile u32, 0x3F20_0028);
 
 comptime {
-    asm(
+    asm (
         \\.section .text.boot // the linker script puts this at the start of the image
         \\.global _start
         \\_start:
@@ -22,9 +13,21 @@ comptime {
 
 export fn simplestMain() noreturn {
     GPFSEL1.* = 0x0004_0000;
-    GPFSET0.* = 0x0001_0000;
 
+    var ledON = true;
     while (true) {
-        // nothing!
+        if (ledON) {
+            GPSET0.* = 0x0001_0000;
+        } else {
+            GPCLR0.* = 0x0001_0000;
+        }
+
+        var i: u32 = 2_500_000;
+        while (i > 0) {
+            GPFSEL1.* = 0x0004_0000;
+            i -= 1;
+        }
+
+        ledON = !ledON;
     }
 }
